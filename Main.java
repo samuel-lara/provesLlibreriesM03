2d6c8122c1e103be5c749bd60d0fd8ff5697d616
iamsamelea <iamsamelea.code@gmail.com>
2022-03-23 11:45:29 +0100

    primer commit activitats llibreries Java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+      int[] myArray = {10, 20, 30, 40, 50 ,60 ,70 ,80, 90};
+      Pantalla.imprimirArray(myArray, 5);
+    }
+  }
---
+++ Pantalla.java
@@ -0,0 +1,7 @@
+public class Pantalla{
+  public static void imprimirArray(int[] vector, int n) {
+    for(int i = 0; i < n; i++){
+      System.out.println(vector[i]);
+    }
+  }
+}
